2f54e13da60285579fd308b9df49c89526d48d2f
Danilo Paes <danilo_paes@pm.me>
2024-09-05 11:49:45 +0200

    adjusting task02

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -1,20 +1,53 @@
+import json
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
+from commons.exception import ApplicationException
 
 _LOG = get_logger('HelloWorld-handler')
 
 
 class HelloWorld(AbstractLambda):
 
-    def validate_request(self, event) -> dict:
-        pass
+    def get_path(self, event):
+        return event.get('rawPath')
+    
+    def get_method(self, event):
+        return (
+            event
+            .get("requestContext", {})
+            .get("http", {})
+            .get("method")
+        )
+
+    def valid_request(self, event) -> dict:
+        is_method_allowed = self.get_method(event) == "GET"
+        is_path_valid = self.get_path(event) == "/hello"
+        return all([is_method_allowed, is_path_valid])
         
     def handle_request(self, event, context):
-        """
-        Explain incoming event here
-        """
-        # todo implement business logic
-        return 200
+        response = {
+            "statusCode": 0,
+            "message": ""
+        }
+        if self.valid_request(event=event):
+            response["statusCode"] = 200
+            response["message"] = "Hello from Lambda"
+        else:
+            response["statusCode"] = 404
+            response["message"] = (
+                "Bad request syntax or unsupported method. "
+                f"Request path: {self.get_path(event)}. "
+                f"HTTP method: {self.get_method(event)}"
+            )
+
+        return {
+            "statusCode": response.get("statusCode"),
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps(response)
+        }
+    
     
 
 HANDLER = HelloWorld()
--- task02/tests/test_hello_world/test_success.py
+++ task02/tests/test_hello_world/test_success.py
@@ -1,8 +1,20 @@
+import json
 from tests.test_hello_world import HelloWorldLambdaTestCase
 
 
 class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), 200)
+        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), {
+            "statusCode": 404,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "statusCode": 404,
+                'message':  'Bad request syntax or unsupported method. Request path: None. '
+                        'HTTP method: None'})
+            }
+            
+        )
 
