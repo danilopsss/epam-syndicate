85ca83b66dfc9e363c5bf3ba74fe7ef3b6ecc8c8
Danilo Paes <danilo_paes@pm.me>
2024-10-02 10:47:43 +0200

    adding task06

---
+++ task06/src/lambdas/audit_producer/handler.py
@@ -0,0 +1,50 @@
+import os
+import uuid
+import json
+import boto3 as aws
+from datetime import datetime
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('AuditProducer-handler')
+
+
+class AuditProducer(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        region = os.environ.get("REGION", "eu-central-1")
+        table_name = os.environ.get('TABLE_NAME')
+        data = {
+            "id": str(uuid.uuid4()), 
+            "itemKey": "CACHE_TTL_SEC",
+            "modificationTime": datetime.now().isoformat(),
+            "updatedAttribute": "value",
+            "oldValue": 3600,
+            "newValue": 1800
+        } 
+
+        dynamodb = aws.resource('dynamodb',  region_name=region)
+        table = dynamodb.Table(table_name)
+        
+        response = table.put_item(Item=data)
+        
+        return {
+            "statusCode": 201,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps(response, indent=4)
+         }
+    
+
+HANDLER = AuditProducer()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
